c3ba583ec612ecec37a48a082fdecd86b92b4a4b
Kim Richmond <kim@richmondstudios.net>
2014-06-26 20:20:26 +0100

    Config section

---
+++ NLogger/NLogger.Tests/ConfigurationTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NLogger.Tests
+{
+    [TestClass]
+    public class ConfigurationTests
+    {
+        private NLoggerSection _config;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _config = (NLoggerSection)ConfigurationManager.GetSection("nLogger");
+        }
+
+        [TestMethod]
+        public void GetFilePath()
+        {
+            Assert.AreEqual("C:\\Logs\\Log.log", _config.FilePath);
+        }
+
+        [TestMethod]
+        public void GetFileSize()
+        {
+            Assert.AreEqual(10, _config.FileSize);
+        }
+
+        [TestMethod]
+        public void GetNumFiles()
+        {
+            Assert.AreEqual(3, _config.NumFiles);
+        }
+
+        [TestMethod]
+        public void GetLogLevel()
+        {
+            Assert.AreEqual(LoggingLevel.Error, _config.LogLevel);
+        }
+    }
+}
---
+++ NLogger/NLogger/NloggerSection.cs
@@ -0,0 +1,36 @@
+﻿using System.Configuration;
+
+namespace NLogger
+{
+    public class NLoggerSection : ConfigurationSection
+    {
+        [ConfigurationProperty("filePath", IsRequired = false)]
+        public string FilePath
+        {
+            get { return (string) this["filePath"]; }
+            set { this["filePath"] = value; }
+        }
+
+        [ConfigurationProperty("numFiles", IsRequired = false, DefaultValue = 10)]
+        public int NumFiles
+        {
+            get { return (int)this["numFiles"]; }
+            set { this["numFiles"] = value; }
+        }
+
+        [ConfigurationProperty("fileSize", IsRequired = false, DefaultValue = 1000)]
+        public int FileSize
+        {
+            get { return (int)this["fileSize"]; }
+            set { this["fileSize"] = value; }
+        }
+
+        [ConfigurationProperty("logLevel", IsRequired = false, DefaultValue = LoggingLevel.Diagnostic)]
+        public LoggingLevel LogLevel
+        {
+            get { return (LoggingLevel) this["logLevel"]; }
+            set { this["logLevel"] = value; }
+        }
+
+    }
+}
