0a8a90d30033dcc5c0b244501a33370027fc9014
Kim Richmond <kim@richmondstudios.net>
2015-01-20 10:37:13 +0000

    Console Writer

---
+++ NLogger/NLogger/ConsoleWriter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NLogger
+{
+    public class ConsoleWriter : ILogWriter
+    {
+        public void AppendText(string text, LoggingLevel level)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
